ba385662595264d9d4fb466fafcfe701e822d279
heyyviv <56256802+heyyviv@users.noreply.github.com>
2020-08-14 14:09:19 +0530

    Initial commit

---
+++ app/src/main/java/com/example/fifty_gram/MainActivity.java
@@ -0,0 +1,125 @@
+package com.example.fifty_gram;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.Transformation;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URI;
+
+import jp.wasabeef.glide.transformations.gpu.PixelationFilterTransformation;
+import jp.wasabeef.glide.transformations.gpu.SepiaFilterTransformation;
+import jp.wasabeef.glide.transformations.gpu.SketchFilterTransformation;
+import jp.wasabeef.glide.transformations.gpu.ToonFilterTransformation;
+
+public class MainActivity extends AppCompatActivity  implements ActivityCompat.OnRequestPermissionsResultCallback{
+    private ImageView imageView;
+    private Bitmap original;
+    TextView location;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        location=(TextView) findViewById(R.id.path);
+
+        imageView = findViewById(R.id.image_view);
+    }
+
+    public void apply(Transformation<Bitmap> filter) {
+        if (original != null) {
+            Glide
+                    .with(this)
+                    .load(original)
+                    .apply(RequestOptions.bitmapTransform(filter))
+                    .into(imageView);
+        }
+    }
+    public void pixelation(View v){
+        apply(new PixelationFilterTransformation());
+
+    }
+
+    public void applySepia(View view) {
+        apply(new SepiaFilterTransformation());
+    }
+
+    public void applyToon(View view) {
+        apply(new ToonFilterTransformation());
+    }
+
+    public void applySketch(View view) {
+        apply(new SketchFilterTransformation());
+    }
+
+    public void choosePhoto(View view) {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.setType("image/*");
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK && data != null) {
+            try {
+                Uri uri = data.getData();
+                ParcelFileDescriptor parcelFileDescriptor =
+                        getContentResolver().openFileDescriptor(uri, "r");
+                FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+                original = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+                parcelFileDescriptor.close();
+                imageView.setImageBitmap(original);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public  void saving(View v){
+        imageView.invalidate();
+        BitmapDrawable drawable=(BitmapDrawable)imageView.getDrawable();
+        Bitmap cur_img=drawable.getBitmap();
+        ContextWrapper wrapper = new ContextWrapper(getApplicationContext());
+        File directory=wrapper.getDir("imageDir", MODE_PRIVATE);
+        directory=new File(directory,"UniqueFilename"+".jpg");
+        try{
+            OutputStream stream=null;
+            stream=new FileOutputStream(directory);
+            cur_img.compress(Bitmap.CompressFormat.JPEG,100,stream);
+            stream.close();
+            stream.flush();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        Uri savedImage=Uri.parse(directory.getAbsolutePath());
+
+        location.setText("Image saved in internal storage.\n" + savedImage);
+
+    }
+}
